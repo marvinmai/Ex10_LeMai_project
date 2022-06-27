f1baff97714ad0758b747eb4a9e5390bdf6826d9
marvin <marvinmai@posteo.de>
2022-06-27 23:06:32 +0200

    Add tests

--- src/test/java/de/unistuttgart/dsass2021/ex10/p6/ClosedHashMapTest.java
+++ src/test/java/de/unistuttgart/dsass2021/ex10/p6/ClosedHashMapTest.java
@@ -13,6 +13,22 @@ public class ClosedHashMapTest {
         map.put(4, 2457);
         map.put(23, 2345);
         map.put(23, 6);
+        map.put(12, 6);
+        map.put(33, 6);
+        map.put(11, 6);
+        map.put(3, 6);
+        map.put(7, 6);
+        map.put(123, 6);
+        map.put(9, 6);
+        map.put(2, 6);
+        map.put(15, 6);
+        map.put(21, 6);
+        map.put(22, 6);
+        map.put(14, 6);
+        map.put(18, 6);
+        map.put(17, 6);
+        map.put(99, 6);
+        map.put(101, 6);
 
         assertTrue(map.containsKey(23));
         assertFalse(map.containsKey(45));
@@ -20,7 +36,16 @@ public class ClosedHashMapTest {
         assertEquals(2457, (int) map.get(4));
         assertEquals(6, (int) map.get(23));
 
-        System.out.println();
+        // high capacity test
+        for (int i = 200; i < 10000000; i++) {
+            map.put(i, i/3);
+        }
+        for (int i = 200; i < 10000000; i++) {
+            assertTrue(map.containsKey(i));
+        }
+        for (int i = 200; i < 10000000; i++) {
+            assertEquals(i/3, (int) map.get(i));
+        }
     }
 
 }
